699f610e4d8ec72d7d370e58ea7d2dd3e27df0aa
VIVEK KUMAR <143505810+Vroy4298@users.noreply.github.com>
2025-12-23 17:40:34 +0530

    fix cors for vercel frontend

--- backend/server.mjs
+++ backend/server.mjs
@@ -18,10 +18,9 @@ const PORT = process.env.PORT || 5000;
 ======================= */
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://land-tax-system.vercel.app",
-  "https://land-tax-system-ten.vercel.app",
-  "https://land-tax-system-g2c5.vercel.app",
-  "https://land-tax-system-1.onrender.com",
+  "https://land-tax-system-g2c5.vercel.app"
+  
+ 
   // add any other Vercel preview domain if needed
 ];
 
